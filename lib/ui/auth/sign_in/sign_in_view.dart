315f3d5b80f2f72101cd959b95410276f9860b9a
generalChang <rhdrnsckdgh0921@naver.com>
2024-08-06 16:51:39 +0900

    :lipstick: 로그인 뷰 추가

--- lib/ui/auth/sign_in/sign_in_view.dart
+++ lib/ui/auth/sign_in/sign_in_view.dart
@@ -8,20 +8,91 @@ class SignInView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(signInViewModelProvider);
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('sign in page'),
-        ),
-        body: const Center(
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('sign In view'),
+            children: <Widget>[
+              TextField(
+                onChanged: (String val) {},
+                onTapOutside: (event) {
+                  FocusManager.instance.primaryFocus?.unfocus();
+                },
+                decoration: InputDecoration(
+                  label: const Text('이메일'),
+                  filled: true,
+                  fillColor: Colors.grey.shade200,
+                  enabledBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                  ),
+                  focusedBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                onChanged: (String val) {},
+                obscureText: true,
+                onTapOutside: (event) {
+                  FocusManager.instance.primaryFocus?.unfocus();
+                },
+                decoration: InputDecoration(
+                  label: const Text('비밀번호'),
+                  filled: true,
+                  fillColor: Colors.grey.shade200,
+                  enabledBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                  ),
+                  focusedBorder: const UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
               SizedBox(
-                height: kToolbarHeight,
-              )
+                width: double.infinity,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        textStyle: const TextStyle(fontSize: 18),
+                        backgroundColor: Colors.orange.shade500,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        padding: const EdgeInsets.all(16)),
+                    onPressed: () {},
+                    child: const Text('로그인 하기')),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        textStyle: const TextStyle(fontSize: 18),
+                        backgroundColor: Colors.redAccent.shade200,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        padding: const EdgeInsets.all(16)),
+                    onPressed: () {},
+                    child: const Text('회원가입 하기')),
+              ),
             ],
           ),
-        ));
+        ),
+      )),
+    );
   }
 }
