8066a376d4958e2f6e981fdd6cb98f99ae61ca10
generalChang <rhdrnsckdgh0921@naver.com>
2024-08-06 23:24:34 +0900

    :art: router redirect logic을 위한 app router interceptor 및 redirect notifier 추가

--- lib/manager/router/app_router.dart
+++ lib/manager/router/app_router.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_best_practice/manager/router/app_router_intercepter.dart';
+import 'package:flutter_best_practice/manager/router/redirect_notifier.dart';
 import 'package:flutter_best_practice/manager/router/routes.dart';
 import 'package:flutter_best_practice/ui/auth/sign_in/sign_in_view.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouterProvider = Provider((ref) {
-  return AppRouter();
+  return AppRouter(
+      appRouterInterceptor: ref.watch(appRouterInterceptorProvider),
+      refreshListenable: ref.watch(redirectNotifierProvider));
 });
 
 class AppRouter {
+  final AppRouterInterceptor _appRouterInterceptor;
+  final Listenable _refreshListenable;
+
+  AppRouter(
+      {required AppRouterInterceptor appRouterInterceptor,
+      required Listenable refreshListenable})
+      : _refreshListenable = refreshListenable,
+        _appRouterInterceptor = appRouterInterceptor;
   static final GlobalKey<NavigatorState> rootNavigatorKey =
       GlobalKey<NavigatorState>();
 
-  final _router = GoRouter(
+  late final _router = GoRouter(
     navigatorKey: rootNavigatorKey,
     initialLocation: '/auth/sign-in',
+    redirect: _appRouterInterceptor.redirect,
+    refreshListenable: _refreshListenable,
     routes: <RouteBase>[
       GoRoute(
           path: Routes.auth.path,
